1341eec529b9691f3db7ba62ced0a587a84fb259
exascape <freshgum.bubbles@protonmail.com>
2023-06-20 17:59:40 +0100

    chore(tests): Make tests run quicker
    Change the Jest configuration to make
    tests run quicker.  Initial testing shows
    this change to be positive, although
    possible future maintainers may encounter
    issues with the default maxWorkers value.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  maxWorkers: 4,
   collectCoverageFrom: ['src/**/*.ts', '!src/**/index.ts', '!src/**/*.interface.ts'],
   globals: {
     'ts-jest': {
