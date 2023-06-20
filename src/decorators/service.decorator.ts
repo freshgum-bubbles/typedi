572f2fd48a064308752a5187f091d5ed6007d5c5
exascape <freshgum.bubbles@protonmail.com>
2023-06-20 03:04:53 +0100

    fix(Service): tighten dependency normalization
    Tighten the dependency normalization in the
    Service decorator to 100% conform to the
    expected overloads.
    This fixes some issues in the prior implementation.
    
    Furthermore, the types for the  single-parameter
    use-case have been tightened up to ensure the
    dependencies key is present.
    See 5ca10c4067600daf8751632d032d6d0e5329e8e9.

--- src/decorators/service.decorator.ts
+++ src/decorators/service.decorator.ts
@@ -60,12 +60,13 @@ export function Service<T = unknown>(options: Omit<ServiceOptions<T>, 'dependenc
  * 
  * @returns A decorator which is then used upon a class.
  */
-export function Service (options: ServiceOptions<Constructable<unknown>>): ClassDecorator;
-export function Service<T>(optionsOrDependencies: Omit<ServiceOptions<T>, 'dependencies'> | ServiceOptions<T> | AnyInjectIdentifier[], maybeDependencies?: AnyInjectIdentifier[]): ClassDecorator {
+export function Service (options: ServiceOptions<Constructable<unknown>> & { dependencies: AnyInjectIdentifier[] }): ClassDecorator;
+export function Service<T>(
+  optionsOrDependencies: Omit<ServiceOptions<T>, 'dependencies'> | ServiceOptions<T> | AnyInjectIdentifier[], 
+  maybeDependencies?: AnyInjectIdentifier[]
+): ClassDecorator {
   return targetConstructor => {
     const { defaultContainer } = ContainerInstance;
-    const firstArgIsArray = Array.isArray(optionsOrDependencies);
-    const secondArgIsArray = Array.isArray(maybeDependencies);
 
     if (optionsOrDependencies == null) {
       // todo: more info in these error messages!!!
@@ -73,39 +74,41 @@ export function Service<T>(optionsOrDependencies: Omit<ServiceOptions<T>, 'depen
     }
 
     /** A list of dependencies resolved from the arguments provided to the function. */
-    let dependencies!: TypeWrapper[];
+    let resolvedDependencies!: AnyInjectIdentifier[];
 
-    if (firstArgIsArray) {
+    if (Array.isArray(optionsOrDependencies)) {
       /** 
        * If our first argument is an array, then the user has not specified any options, 
        * and has instead filled the slot with a list of dependencies. 
        */
-      dependencies = optionsOrDependencies?.map?.(resolveToTypeWrapper) ?? [];
-    } else if (!firstArgIsArray && !secondArgIsArray) {
-      /**
-       * The first argument is of type `ServiceOptions<T>`.
-       * We can access its "dependencies" property and then map the unwrapped types.
-       */
-      dependencies = (optionsOrDependencies as ServiceOptions<NewableFunction>).dependencies?.map(resolveToTypeWrapper) ?? [];
-    } else if (secondArgIsArray) {
+      resolvedDependencies = optionsOrDependencies;
+    } else if (Array.isArray(maybeDependencies)) {
       /** 
        * Alternatively, they may have specified the dependencies as the last argument.
        * In that case, we'll try to map over the dependencies (if they exist.)
        */
-      dependencies = maybeDependencies.map(resolveToTypeWrapper);
+      resolvedDependencies = maybeDependencies;
+    } else if ('dependencies' in optionsOrDependencies) {
+      /**
+       * The first argument is of type `ServiceOptions<T>` with dependencies.
+       * We can access its "dependencies" property and then map the unwrapped types.
+       */
+      resolvedDependencies = (optionsOrDependencies as any).dependencies;
     }
 
-    if (!dependencies) {
+    if (!resolvedDependencies) {
       /** At this point we have exhausted all options, so throw. */
       throw new Error('The dependencies provided were not able to be resolved.');
     }
+
+    const wrappedDependencies = resolvedDependencies.map(resolveToTypeWrapper);
 
     /**
      * A default list of options for this service.
      * These are used when the options are not explicitly provided to the function.
      * However, if options are passed, these are merged in as defaults.
      */
-    const defaultOptions: ServiceMetadata<T> & { container: ContainerInstance } = {
+    let metadata: Omit<ServiceMetadata<T>, 'dependencies'> & { container: ContainerInstance } = {
       id: targetConstructor,
       type: targetConstructor as unknown as Constructable<T>,
       multiple: false,
@@ -113,15 +116,18 @@ export function Service<T>(optionsOrDependencies: Omit<ServiceOptions<T>, 'depen
       scope: 'container',
       value: EMPTY_VALUE,
       factory: undefined,
-      dependencies,
       container: defaultContainer
     };
 
-    /**
-     * Merge the options with the default options, if any were provided.
-     */
-    const metadata: ServiceMetadata<T> & { container: ContainerInstance } =
-      firstArgIsArray ? { ...defaultOptions, dependencies } : { ...defaultOptions, ...optionsOrDependencies };
+    if (!Array.isArray(optionsOrDependencies)) {
+      metadata = { ...metadata, ...optionsOrDependencies };
+
+      /**
+       * Remove the decorators from the options forcibly.
+       * Otherwise, our type-wrapped dependencies passed to ContainerInstance.set are ignored.
+       */
+      delete (metadata as any).dependencies;
+    }
 
     const { id, container } = metadata;
 
@@ -138,7 +144,7 @@ export function Service<T>(optionsOrDependencies: Omit<ServiceOptions<T>, 'depen
      * Check any available eager types immediately, so we can quickly raise an error
      * if they are invalid, instead of when the service is injected.
      */
-    dependencies.forEach((wrapper, index) => {
+    wrappedDependencies.forEach((wrapper, index) => {
       if (wrapper.isFactory) {
         return;
       }
@@ -161,6 +167,6 @@ export function Service<T>(optionsOrDependencies: Omit<ServiceOptions<T>, 'depen
      * By default, this is set to `defaultContainer`.
      * Therefore, it will be bound to that.
      */
-    container.set(metadata, metadata.dependencies);
+    container.set(metadata, wrappedDependencies);
   };
 }
