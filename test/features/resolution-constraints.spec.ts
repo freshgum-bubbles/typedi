47ffc5490974ae79d37e2c17f43b0adacbfd95c8
exascape <freshgum.bubbles@protonmail.com>
2023-06-24 17:12:47 +0100

    test(constraints): add tests for operations
    Add tests for the constraints API to the suite.
    
    Currently, the tests for Many are disabled, as they
    are encountering a strange runtime error.

--- test/features/resolution-constraints.spec.ts
+++ test/features/resolution-constraints.spec.ts
@@ -1,17 +1,172 @@
+import { Container, ContainerInstance, Many, Optional, Self, SkipSelf } from '../../src/index';
+import { Service } from '../../src/decorators/service.decorator';
+
 describe('Resolution Constraints', function () {
-    describe('Optional', function () {
+  beforeEach(() => Container.reset({ strategy: 'resetValue' }));
+
+  describe('Optional', function () {
+    it('should work correctly on its own', function () {
+      class ThisWillNotResolve {}
+
+      @Service([[ThisWillNotResolve, Optional()]])
+      class MyService {
+        constructor(public receivedValue: unknown) {}
+      }
+
+      expect(Container.get(MyService).receivedValue).toStrictEqual(null);
+    });
+
+    it('should defer back to the actual value if it is present', function () {
+        @Service([])
+        class ThisWillResolve { }
+
+        @Service([[ThisWillResolve, Optional()]])
+        class MyService {
+            constructor (public receivedValue: unknown) { }
+        }
+
+        expect(Container.get(MyService).receivedValue).toBeInstanceOf(ThisWillResolve);
+    });
+  });
+
+  describe('SkipSelf', function () {
+    it('should skip the value in the current container', function () {
+        const childContainer = Container.of(Symbol());
+
+        const mockNeverRunsFn = jest.fn();
+        const mockShouldRunFn = jest.fn();
+
+        @Service({ container: childContainer }, [])
+        class ThisShouldNeverRun {
+            constructor () {
+                mockNeverRunsFn();
+            }
+        }
+
+        /** This will be bound to the default container, which is the parent of childContainer. */
+        @Service({ id: ThisShouldNeverRun }, [ ])
+        class ThisShouldRun {
+            constructor () {
+                mockShouldRunFn();
+            }
+        }
+
+        @Service([
+            [ThisShouldNeverRun, SkipSelf()]
+        ])
+        class MyService {
+            constructor (public receivedValue: unknown) { }
+        }
+
+        const myService = childContainer.get(MyService);
+
+        expect(myService.receivedValue).toBeInstanceOf(ThisShouldRun);
+        expect(mockNeverRunsFn).not.toHaveBeenCalled();
+        expect(mockShouldRunFn).toHaveBeenCalledTimes(1);
+    });
+
+    it('should immediately fail if the container does not have a parent', function () {
+        const containerWithNoParent = ContainerInstance.of(Symbol(), null);
+
+        @Service([])
+        class AnotherService { }
+
+        @Service({ container: containerWithNoParent }, [
+            [AnotherService, SkipSelf()]
+        ])
+        class MyService { }
+
+        expect(() => containerWithNoParent.get(MyService)).toThrowError();
+    });
+  });
+
+  describe('Many', function () {
+    it.skip('should return the many instances of the given identifier', function () {
+        @Service({ multiple: true }, [ ])
+        class Dependency { }
+
+        @Service([[Dependency, Many()]])
+        class MyService {
+            constructor (public receivedValue: unknown) { }
+        }
+
+        expect(() => Container.get(MyService)).not.toThrowError();
+        expect(Container.get(MyService).receivedValue).toMatchObject([Dependency]);
+    });
+
+    it.skip('should throw an error if the identifier cannot be found', function () {
 
     });
 
-    describe('SkipSelf', function () {
+    it.skip('should return null when combined with Optional and the identifier cannot be found', function () {
 
     });
 
-    describe('Many', function () {
+    it.skip('should work correctly with Skip', function () {
 
     });
 
-    describe('Self', function () {
+    it.skip('should work correctly with SkipSelf', function () {
+
+    });
+  });
+
+  describe('Self', function () {
+    it('should error if ID not found in current container, even if its parent has it', function () {
+        /** 90% of this was copied from above, with some small modifications. Why re-write it? :-) */
+        const childContainer = Container.ofChild(Symbol());
+        const mockNeverRunsFn = jest.fn().mockName("Should Not Run");
         
+        @Service([])
+        class ThisShouldNeverRun {
+            constructor () {
+                mockNeverRunsFn();
+            }
+        }
+
+        @Service({ container: childContainer }, [
+            [ThisShouldNeverRun, Self()]
+        ])  
+        class MyService {
+            constructor () { }
+        }
+
+        expect(() => childContainer.get(MyService)).toThrowError();
+        // expect(mockNeverRunsFn).not.toHaveBeenCalled();
     });
+
+    it('should return null when combined with Optional and the identifier cannot be found', function () {
+        const childContainer = Container.ofChild(Symbol());
+
+        @Service([ ])
+        class Dependency {
+            constructor () {}
+        }
+
+        @Service({ container: childContainer }, [[Dependency, Self() | Optional()]])
+        class MyService {
+            constructor (public receivedValue: unknown) { }
+        }
+
+        expect(() => childContainer.get(MyService)).not.toThrowError();
+        expect(childContainer.get(MyService).receivedValue).toBeNull();
+    });
+
+    it('should find locally-resolvable identifiers', function () {
+        const childContainer = Container.ofChild(Symbol());
+
+        @Service({ container: childContainer }, [ ])
+        class Dependency {
+            constructor () {}
+        }
+
+        @Service({ container: childContainer }, [[Dependency, Self()]])
+        class MyService {
+            constructor (public receivedValue: unknown) { }
+        }
+
+        expect(() => childContainer.get(MyService)).not.toThrowError();
+        expect(childContainer.get(MyService).receivedValue).toBeInstanceOf(Dependency);
+    });
+  });
 });
