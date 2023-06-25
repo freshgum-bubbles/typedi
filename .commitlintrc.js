444e6dafbac72ff5d1a99aa39305c71a0a317c38
exascape <freshgum.bubbles@protonmail.com>
2023-06-25 02:56:30 +0100

    chore(git): add commitlint configuration

---
+++ .commitlintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    extends: ['@commitlint/config-angular'],
+    rules: {
+        /** Reference: <https://commitlint.js.org/#/reference-rules> */
+        'type-enum': [2, 'always', [
+            'build',
+            'chore',
+            'fix',
+            'feat',
+            'style',
+            'merge',
+            'docs',
+            'perf',
+            'refactor',
+            'test'
+        ]],
+        'subject-empty': [2, 'never'],
+        'subject-full-stop': [2, 'never']
+    }
+};
