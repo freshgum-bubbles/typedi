94d4ff69be2e3d2bcb0a4e2fc0dd630ccb0741ef
exascape <freshgum.bubbles@protonmail.com>
2023-06-26 03:32:12 +0100

    refactor(website): update configuration management
    Add a set of custom fields alongside a decorator that
    returns the custom fields in a typed manner.
    This is required as the original decorator from Docusaurus
    types customFields to a generic Record type.

---
+++ website/src/app-config.ts
@@ -0,0 +1,31 @@
+import { DocusaurusConfig, DocusaurusContext } from "@docusaurus/types";
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+
+interface APIReferenceCustomField {
+    location: string;
+    link: string;
+}
+
+/**
+ * Create a custom type extending the default Docusuaurus
+ * config type to make using custom fields easier.
+ */
+export interface AppConfig extends DocusaurusConfig {
+    customFields: {
+        apiReference: APIReferenceCustomField;
+    };
+}
+
+export type AppConfigCustomFields = AppConfig['customFields'];
+
+export interface AppContext extends DocusaurusContext {
+    siteConfig: AppConfig;
+}
+
+export function useAppContext(...args: Parameters<typeof useDocusaurusContext>): AppContext {
+    /**
+     * While the `useDocusaurusContext` hook does not *currently* take arguments,
+     * we pass them in anyway to ensure compatibility with any future renditions which do.
+     */
+    return useDocusaurusContext(...args) as unknown as AppContext;
+}
