82dcf6c5f1d42ce8578db30e6c6c685048dd110c
exascape <freshgum.bubbles@protonmail.com>
2023-06-26 03:34:03 +0100

    docs(website): update branding

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -86,11 +86,11 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
+        title: 'TypeDI',
+        // logo: {
+        //   alt: 'My Site Logo',
+        //   src: 'img/logo.svg',
+        // },
         items: [
           {
             type: 'docSidebar',
