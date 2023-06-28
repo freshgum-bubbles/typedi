bf87b1b6fb37ef54cbd63fd6a5c471aafba619f6
exascape <freshgum.bubbles@protonmail.com>
2023-06-29 00:46:47 +0100

    chore(docs): update TypeDoc configuration

--- typedoc.config.js
+++ typedoc.config.js
@@ -1,15 +1,20 @@
+const { repository, name } = require('./package.json');
+
 /** @type {import('typedoc').TypeDocOptions} */
 var configuration = {
     "entryPoints": [
         "./src/index.ts"
     ],
+    "gitRemote": repository,
     "plugin": [
-        /** Generate plain Markdown to be consumed by Docusaurus. */
-        "typedoc-plugin-markdown"
+        "@mxssfd/typedoc-theme"
     ],
+    "theme": "my-theme",
     "readme": "README.md",
     "tsconfig": "tsconfig.typedoc.json",
-    "out": "./website/docs/api-reference/"
+    "out": "./website/static/api-reference/",
+    "pretty": true,
+    "titleLink": repository.url,
 }
 
 module.exports = configuration;
