e2bf7e7cd00876eeed830078b4d98204349bdc58
freshgum-bubbles <freshgum.bubbles@protonmail.com>
2023-06-29 13:10:15 +0100

    chore(scripts): make NPM release dry run & force
    This is useful for testing the NPM script without
    creating any further versions.

--- scripts/release_to_npm.sh
+++ scripts/release_to_npm.sh
@@ -2,4 +2,4 @@ cp LICENSE build/LICENSE
 cp README.md build/README
 
 jq 'del(.devDependencies) | del(.scripts)' package.json > build/package.json
-pnpm publish ./build/
+pnpm publish --dry-run ./build/ --access public --no-git-checks --force
