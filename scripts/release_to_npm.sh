9448158cbaa0c2e9d04c96c5e630ef1429b35079
freshgum-bubbles <freshgum.bubbles@protonmail.com>
2023-06-29 19:07:53 +0100

    fix(scripts): make NPM release script copy README
    Fix the NPM release script so it copies the README
    to README.md instead of README.
    NPM can apparently only detect files explicitly
    named README.md.

--- scripts/release_to_npm.sh
+++ scripts/release_to_npm.sh
@@ -1,5 +1,5 @@
 cp LICENSE build/LICENSE
-cp README.md build/README
+cp README.md build/README.md   
 
 jq 'del(.devDependencies) | del(.scripts)' package.json > build/package.json
 pnpm publish --access public --no-git-checks --force
