70d1e6e6637f0d88b5dd7e8542ca3a7ed3fcb882
exascape <freshgum.bubbles@protonmail.com>
2023-06-29 01:09:21 +0100

    test(HostContainer): add stringent testing

---
+++ test/features/host-container.spec.ts
@@ -0,0 +1,54 @@
+import { Container, HostContainer, Token, Service, ContainerInstance } from '../../src/index';
+
+describe('HostContainer', function () {
+  it('returns a token', function () {
+    expect(HostContainer()).toBeInstanceOf(Token);
+  });
+
+  it('has a constant return value with subsequent calls', function () {
+    for (let index = 0; index >= 10; index++) {
+      expect(HostContainer()).toStrictEqual(Container);
+    }
+  });
+
+  /**
+   * Just to be extra sure it resolves correctly in all situations,
+   * we make use of describe.each to run over it being used in three cases:
+   *   - The default container
+   *   - An orphaned container
+   *   - A child container
+   */
+  describe.each([
+    { name: 'Default Container', container: Container },
+    { name: 'Orphaned Container', container: ContainerInstance.of(Symbol(), null) },
+    { name: 'Child Container', container: Container.ofChild(Symbol()) },
+  ])('$name: HostContainer usage', ({ container }) => {
+    it('returns the host container', function () {
+      expect(container.get(HostContainer())).toStrictEqual(container);
+    });
+
+    it('always resolves', function () {
+      expect(container.has(HostContainer())).toStrictEqual(true);
+      expect(container.get(HostContainer())).toStrictEqual(container);
+    });
+
+    it('resolves as a service dependency', function () {
+      @Service({ container }, [HostContainer()])
+      class MyService {
+        constructor(public receivedValue: any) {}
+      }
+
+      const myService = container.get(MyService);
+      expect(myService.receivedValue).toStrictEqual(container);
+    });
+
+    it('returns true when passed to .has', function () {
+      expect(container.has(HostContainer())).toStrictEqual(true);
+    });
+
+    it('does not resolve in "many" functions', function () {
+      expect(() => container.getMany(HostContainer())).toThrowError();
+      expect(container.getManyOrNull(HostContainer())).toBeNull();
+    });
+  });
+});
