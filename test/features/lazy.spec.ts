3845b5b25aa08210781edc99f3cbd50b1c1fda9c
freshgum-bubbles <freshgum.bubbles@protonmail.com>
2023-07-01 03:18:34 +0100

    test(Lazy): add tests for Lazy helper function

---
+++ test/features/lazy.spec.ts
@@ -0,0 +1,33 @@
+import {
+    Container,
+    ContainerInstance,
+    Many,
+    Optional,
+    Self,
+    SkipSelf,
+    ResolutionConstraintFlag,
+    Lazy,
+    Service
+} from '../../src/index';
+
+describe('Lazy()', function () {
+    let emptyFunction = () => {};
+    it('should accept a function and return an object', () => {
+        expect(() => Lazy(emptyFunction)).not.toThrowError();
+        expect(Lazy(emptyFunction)).toBeInstanceOf(Object);
+    });
+
+    it('should be usable as a service dependency', () => {
+        @Service([ ])
+        class AnotherService { }
+
+        @Service([
+            Lazy(() => AnotherService)
+        ])
+        class MyService {
+            constructor (public receivedValue: unknown) { }
+        }
+
+        expect(Container.get(MyService).receivedValue).toBeInstanceOf(AnotherService);
+    });
+});
