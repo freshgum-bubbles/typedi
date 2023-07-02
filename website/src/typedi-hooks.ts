5b90f905794e84aeea779d1c8e1a217f0a913470
freshgum-bubbles <freshgum.bubbles@protonmail.com>
2023-07-02 01:14:35 +0100

    feat(website): add TypeDI hooks

---
+++ website/src/typedi-hooks.ts
@@ -0,0 +1,14 @@
+import { Container, ServiceIdentifier, ContainerIdentifier } from '@typed-inject/injector';
+import { useMemo } from 'react';
+
+export function useService<T> (id: ServiceIdentifier, container = Container) {
+    const value = useMemo(() => container.getOrNull(id), [id]);
+
+    return [value as T] as const;
+}
+
+export function useContainer (id: ContainerIdentifier) {
+    const value = useMemo(() => Container.ofChild(id), [id]);
+
+    return [value] as const;
+}
