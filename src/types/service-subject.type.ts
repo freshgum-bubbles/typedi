540f676f3639cf4facb23089fd876b06c734c0a7
freshgum-bubbles <freshgum.bubbles@protonmail.com>
2023-07-05 11:24:07 +0100

    refactor(Service): replace ServiceSubject with ServiceWithDependencies
    Furthermore, simplify the type-resolution algorithm and remove its
    dependency on arrays with a fixed number of items.
    The new implementation is compatible with arrays of any size.
    
    A major advantage of this implementation is that it **dramatically**
    simplifies the implementation of dependency type-checking,
    while also removing the need for multiple Service / JSService overloads.
    Overall, this means much less code duplication.

--- src/types/service-subject.type.ts
+++ src/types/service-subject.type.ts
@@ -1,4 +1,3 @@
-import { Service } from '../decorators/service.decorator';
 import {
   AnyServiceDependency,
   DependencyPairWithConfiguration,
@@ -6,34 +5,20 @@ import {
 import { Token } from '../token.class';
 import { LazyReference } from './lazy-reference.type';
 import { ServiceIdentifier } from './service-identifier.type';
-import { TypedConstructable } from './typed-constructable.type';
 
 /**
  * @fileoverview @ignore
- * The code below is the best we can reasonably do in the current version of TypeScript.
- * You can't easily map over an array type and, without explicitly typing it as a tuple,
- * the actual type information of individual elements is lost.
- *
- * The individual array types below are for checking the length of the tuple passed into
- * the TDeps generic slot.  We have to iterate the tuple, checking its length manually,
- * in order to run any processing on its individual elements.
- *
- * The main focus of the implementation is below, in the `UnpackServiceDependency` type.
- * This type transforms dependencies into their actual values.
- *
- * We also provide a `ServiceSubject` type, which constructs a class type from a list of 
- * dependencies as a tuple.
- * It accepts two generic parameters:
- *   - T: This is the return type of the returned class.
- *   - TDeps: These are the dependencies of the class, 
- *     that are then mapped to the new class' constructor arguments.
+ * The premise of this file is to allow for the conversion of service dependency lists into types which are appropriate
+ * for consuming as part of the service's constructor.
+ * 
+ * The main focus of the implementation is below, in the `UnpackServiceDependency` type. This type transforms
+ * dependencies into their actual values.
  *
  * For a constructor passed into `ServiceSubject`, an "expected type" is returned.
  * 
  * Consider the following example:
  * ```ts
- * @Service([AnotherService])
- * class MyService {}
+ * @Service([AnotherService]) class MyService {}
  * ```
  *
  * In this case, as the `Service` overloads statically determines the class' dependencies,
@@ -52,9 +37,9 @@ import { TypedConstructable } from './typed-constructable.type';
  * }
  * ```
  * 
- * If you look below, we also guard against built-in types surfacing into type design.
- * For instance, if a class uses Number as a dependency, the accompanying argument in
- * the constructor would be `number` instead of the discouraged `Number` type.
+ * If you look below, we also guard against built-in types surfacing into type design. For instance, if a class uses
+ * Number as a dependency, the accompanying argument in the constructor would be `number` instead of the discouraged
+ * `Number` type.
  * 
  * Consider the following example:
  * ```ts
@@ -64,32 +49,23 @@ import { TypedConstructable } from './typed-constructable.type';
  * }
  * ```
  * 
- * Instead of using `Number` in the constructor, we're able to use `number` in its place.
- * This is because, for each dependency, we check if the type matches any of the built-in
- * constructors, such as Number, String and Object.
- * If they do, they're cast to their underlying native types. In the case of Number such
- * as in the above example, that type is cast to `number`.
- * The type compiler below is specially designed to accommodate this use-case.
+ * Instead of using `Number` in the constructor, we're able to use `number` in its place. This is because, for each
+ * dependency, we check if the type matches any of the built-in constructors, such as Number, String and Object. If they
+ * do, they're cast to their underlying native types. In the case of Number such as in the above example, that type is
+ * cast to `number`. The type compiler below is specially designed to accommodate this use-case.
  * 
  * ---
  * 
- * One note is that in the implementation, the expected type's return value is `unknown`.
- * This is fine, as the return types of decorators doesn't actually matter to TypeScript.
- * This makes the implementation a little bit simpler.
+ * One note is that in the implementation, the expected type's return value is `unknown`. This is fine, as the return
+ * types of decorators doesn't actually matter to TypeScript. This makes the implementation a little bit simpler.
  *
- * The advantage of this overall approach is that it mostly keeps the type-checking decoupled from 
- * the Service decorator implementation.  The actual type-checking is performed in this file.
- * Furthermore, we get type-checking basically for free; this doesn't impact the size
- * of the final bundle at all. Sweet!
+ * The advantage of this overall approach is that it mostly keeps the type-checking decoupled from the Service decorator
+ * implementation.  The actual type-checking is performed in this file. Furthermore, we get type-checking basically for
+ * free; this doesn't impact the size of the final bundle at all. Sweet!
  *
- * One notable drawback of this implementation is that, without modifications to TypeScript,
- * we've absolutely no way to force `Optional`-marked parameters to possibly expect a null type.
- * In the case of our resolution constraint bitmask, it's cast to a number internally.
- * We can't check individual flags against it.
- * 
- * Another drawback is we have to craft *each* overload by hand.
- * Take a look at the Service decorator and you'll understand.
- * It's currently at 500 lines.
+ * One notable drawback of this implementation is that, without modifications to TypeScript, we've absolutely no way to
+ * force `Optional`-marked parameters to possibly expect a null type. In the case of our resolution constraint bitmask,
+ * it's cast to a number internally. We can't check individual flags against it.
  */
 
 /**
@@ -178,38 +154,27 @@ export type UnpackServiceDependency<T extends AnyServiceDependency> =
   T extends LazyReference<infer U extends ServiceIdentifier> ? MapBuiltInToNativeType<UnpackServiceDependency<U>> :
   never;
 
-/** @ignore */ type Array1 = [any];
-/** @ignore */ type Array2 = [any, any];
-/** @ignore */ type Array3 = [any, any, any];
-/** @ignore */ type Array4 = [any, any, any, any];
-/** @ignore */ type Array5 = [any, any, any, any, any];
-/** @ignore */ type Array6 = [any, any, any, any, any, any];
-/** @ignore */ type Array7 = [any, any, any, any, any, any, any];
-/** @ignore */ type Array8 = [any, any, any, any, any, any, any, any];
-/** @ignore */ type Array9 = [any, any, any, any, any, any, any, any, any];
-/** @ignore */ type Array10 = [any, any, any, any, any, any, any, any, any, any];
-
 /**
- * A shortcut for {@see UnpackServiceDependency}.
- * @ignore
+ * Create an appropriate type for a decorator's subject constructor,
+ * where T is equal to the type of instance expected, and TDeps is
+ * equal to its dependencies.
  */
-type USD<T extends AnyServiceDependency> = UnpackServiceDependency<T>;
+export type ServiceWithDependencies<TDeps extends readonly unknown[], T = unknown> =
+  /**
+   * We can't use the TypedConstructable type here as otherwise,
+   * TypeScript complains that the type instantiaton is "excessively deep".
+   * 
+   * While we're not sure how / why this fixes the issue, it does.
+   * 
+   * Full TS error text for reference:
+   *   Type instantiation is excessively deep and possibly infinite.ts(2589)
+   */
+  (...args: UnpackDependencies<TDeps>) => T;
 
 /**
- * The implementation of this is explained in the file overview.
- * @ignore
- *
- * The "never" for non-fitting tuples is OK, as we only use this in {@see Service} for 10 dependencies or less.
- */ // prettier-ignore
-export type ServiceSubject <T extends unknown, TDeps extends readonly AnyServiceDependency[]> =
-    TDeps extends Array1 ? TypedConstructable<T, [USD<TDeps[0]>]> : /* 1 args */
-    TDeps extends Array2 ? TypedConstructable<T, [USD<TDeps[0]>, USD<TDeps[1]>]> : /* 2 args */
-    TDeps extends Array3 ? TypedConstructable<T, [USD<TDeps[0]>, USD<TDeps[1]>, USD<TDeps[2]>]> : /* 3 args */
-    TDeps extends Array4 ? TypedConstructable<T, [USD<TDeps[0]>, USD<TDeps[1]>, USD<TDeps[2]>, USD<TDeps[3]>]> : /* 4 args */
-    TDeps extends Array5 ? TypedConstructable<T, [USD<TDeps[0]>, USD<TDeps[1]>, USD<TDeps[2]>, USD<TDeps[3]>, USD<TDeps[4]>]> : /* 5 args */
-    TDeps extends Array6 ? TypedConstructable<T, [USD<TDeps[0]>, USD<TDeps[1]>, USD<TDeps[2]>, USD<TDeps[3]>, USD<TDeps[4]>, USD<TDeps[5]>]> : /* 6 args */
-    TDeps extends Array7 ? TypedConstructable<T, [USD<TDeps[0]>, USD<TDeps[1]>, USD<TDeps[2]>, USD<TDeps[3]>, USD<TDeps[4]>, USD<TDeps[5]>, USD<TDeps[6]>]> : /* 7 args */
-    TDeps extends Array8 ? TypedConstructable<T, [USD<TDeps[0]>, USD<TDeps[1]>, USD<TDeps[2]>, USD<TDeps[3]>, USD<TDeps[4]>, USD<TDeps[5]>, USD<TDeps[6]>, USD<TDeps[7]>]> : /* 8 args */
-    TDeps extends Array9 ? TypedConstructable<T, [USD<TDeps[0]>, USD<TDeps[1]>, USD<TDeps[2]>, USD<TDeps[3]>, USD<TDeps[4]>, USD<TDeps[5]>, USD<TDeps[6]>, USD<TDeps[7]>, USD<TDeps[8]>]> : /* 9 args */
-    TDeps extends Array10 ? TypedConstructable<T, [USD<TDeps[0]>, USD<TDeps[1]>, USD<TDeps[2]>, USD<TDeps[3]>, USD<TDeps[4]>, USD<TDeps[5]>, USD<TDeps[6]>, USD<TDeps[7]>, USD<TDeps[8]>, USD<TDeps[9]>]> : /* 10 args */
-    never;
+ * Unpack a list of dependencies for a service, mapping each one
+ * to an appropriate type for the service's constructor.
+ */
+export type UnpackDependencies<TDeps extends readonly any[]> = typeof Array['prototype'] & {
+  [TIndex in keyof Pick<TDeps, number>]: UnpackServiceDependency<TDeps[TIndex]>;
+};
