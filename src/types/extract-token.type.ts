aed2dba4b83cab03afd05aed6815ffd36e32194a
freshgum-bubbles <freshgum.bubbles@protonmail.com>
2023-07-06 20:19:42 +0100

    feat(types): Add ExtractToken type

---
+++ src/types/extract-token.type.ts
@@ -0,0 +1,13 @@
+import { Token } from "../token.class";
+
+/**
+ * Extract the type of a Token.
+ * 
+ * @example
+ * ```ts
+ * const token = new Token<string>();
+ * const value: ExtractToken<typeof token> = "Hello!";
+ * ```
+ * 
+ */
+export type ExtractToken<T> = T extends Token<infer U> ? U : never;
