0bcd79c6767738be5d1774e986d55930fa7a7ff5
freshgum-bubbles <freshgum.bubbles@protonmail.com>
2023-07-09 00:00:31 +0100

    refactor(types): move PickRequired into separate file

--- src/types/helper-types.type.ts
+++ src/types/helper-types.type.ts
@@ -3,9 +3,3 @@
  */
 export type PartialPick<TTarget, TKeys extends keyof TTarget> = Partial<TTarget> & Pick<TTarget, TKeys>;
 
-/**
- * From TTarget, make keys in TKeys required.
- */
-export type PickRequired<TTarget, TKeys extends keyof TTarget> = {
-    -readonly [key in TKeys]: TTarget[key];
-}
