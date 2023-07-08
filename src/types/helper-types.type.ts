5fa56ef3fbae90885f1e1d598464a037cc919542
freshgum-bubbles <freshgum.bubbles@protonmail.com>
2023-07-08 23:58:48 +0100

    feat(types): add PickRequired type

--- src/types/helper-types.type.ts
+++ src/types/helper-types.type.ts
@@ -2,3 +2,10 @@
  * From TTarget, make only keys in TKeys required, with the rest being optional.
  */
 export type PartialPick<TTarget, TKeys extends keyof TTarget> = Partial<TTarget> & Pick<TTarget, TKeys>;
+
+/**
+ * From TTarget, make keys in TKeys required.
+ */
+export type PickRequired<TTarget, TKeys extends keyof TTarget> = {
+    -readonly [key in TKeys]: TTarget[key];
+}
