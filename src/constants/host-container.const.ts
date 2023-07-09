83c3225f24646c0a7d71921b76490486c6745fc9
freshgum-bubbles <freshgum.bubbles@protonmail.com>
2023-07-09 02:18:46 +0100

    docs(*): revise TSDoc documentation

--- src/constants/host-container.const.ts
+++ src/constants/host-container.const.ts
@@ -4,5 +4,7 @@ import { Token } from '../token.class';
 /**
  * A special identifier which can be used to get the container
  * the service is currently being executed under.
+ * 
+ * @see {@link HostContainer}
  */
 export const HOST_CONTAINER = new Token<ContainerInstance>('Host Container');
