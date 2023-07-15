73e275977d48bd43533332097954c683632148dc
freshgum-bubbles <freshgum.bubbles@protonmail.com>
2023-07-15 20:26:59 +0100

    fix(Service)!: re-type service options to comply with spec
    BREAKING CHANGE: This will most likely impact consumers of the library who rely
    on the structure of the ServiceOptions type.
    
    The ServiceOptions type has been re-typed
    to make it function correctly, as according to
    the specification both here and upstream.
    It now requires either factory, value or type
    to be present in the list of service options.
    
    The new type still enforces the optional
    inclusion of dependencies, encoded as
    an array of service dependencies (which
    haven't been type-wrapped yet.)

--- src/interfaces/service-options.interface.ts
+++ src/interfaces/service-options.interface.ts
@@ -1,16 +1,14 @@
 import { ServiceMetadata } from './service-metadata.interface';
 import { AnyServiceDependency } from './service-dependency.interface';
+import { RequireExactlyOne } from 'type-fest';
+
+type OptionalKeys = 'type' | 'factory' | 'value';
 
 /**
  * The public ServiceOptions is partial object of ServiceMetadata and either one
  * of the following is set: `type`, `factory`, `value` but not more than one.
  */
-export type ServiceOptions<T = unknown> = (
-  | (
-    // THISISFINE: use type-fest RequireAtLeastOne
-      | Omit<Partial<ServiceMetadata<T>>, 'type' | 'factory'>
-      | Omit<Partial<ServiceMetadata<T>>, 'value' | 'factory'>
-      | Omit<Partial<ServiceMetadata<T>>, 'value' | 'type'>
-    )
-  | Pick<Partial<ServiceMetadata<T>>, 'factory'>
-) & { dependencies?: AnyServiceDependency[] };
+export type ServiceOptions<T = unknown> =
+  Partial<RequireExactlyOne<ServiceMetadata<T>, OptionalKeys>> &
+  Partial<Omit<ServiceMetadata<T>, OptionalKeys>> &
+  { dependencies?: AnyServiceDependency[] };
