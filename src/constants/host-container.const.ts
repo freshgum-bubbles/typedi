6b9998427386de9f0621230f66529f2d0af70219
freshgum-bubbles <freshgum.bubbles@protonmail.com>
2023-07-15 03:18:05 +0100

    docs(HostContainer): add example to TSDoc

--- src/constants/host-container.const.ts
+++ src/constants/host-container.const.ts
@@ -5,6 +5,17 @@ import { Token } from '../token.class';
  * A special identifier which can be used to get the container
  * the service is currently being executed under.
  * 
+ * @example
+ * Here is an example:
+ * ```ts
+ * @Service([
+ *   HostContainer()
+ * ])
+ * class MyService {
+ *   constructor (private container: ContainerInstance) { }
+ * }
+ * ```
+ * 
  * @see {@link HostContainer}
  */
 export const HOST_CONTAINER = new Token<ContainerInstance>('Host Container');
