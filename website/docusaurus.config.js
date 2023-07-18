29b6bfc2226d7f307d92999d652d21be96369463
freshgum-bubbles <freshgum.bubbles@protonmail.com>
2023-07-18 14:38:00 +0100

    docs(website)!: update name to TypeDI++

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -101,7 +101,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'TypeDI',
+        title: 'TypeDI++',
         // logo: {
         //   alt: 'My Site Logo',
         //   src: 'img/logo.svg',
