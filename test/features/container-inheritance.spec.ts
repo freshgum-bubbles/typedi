b7338b94996775dd6f4a06cddf8c8dec1bdb96ad
freshgum-bubbles <freshgum.bubbles@protonmail.com>
2023-07-18 17:25:59 +0100

    style(tests): run Prettier

--- test/features/container-inheritance.spec.ts
+++ test/features/container-inheritance.spec.ts
@@ -1,42 +1,40 @@
 import { Container, Token } from '../../src/index';
 
 describe('Child Containers', () => {
-    function createDeepContainerTree (levels: number, root = Container) {
-        let container = root;
-        let index = 0;
+  function createDeepContainerTree(levels: number, root = Container) {
+    let container = root;
+    let index = 0;
 
-        while (index <= levels) {
-            container = container.ofChild(Symbol());
-            index++;
-        }
-
-        return container;
+    while (index <= levels) {
+      container = container.ofChild(Symbol());
+      index++;
     }
 
-    function createArrayOfNumbers (end: number) {
-        const numbers: number[] = [ ];
-        let index = 0;
+    return container;
+  }
 
-        while (index <= end) {
-            numbers.push(index);
-            index++;
-        }
+  function createArrayOfNumbers(end: number) {
+    const numbers: number[] = [];
+    let index = 0;
 
-        return numbers;
+    while (index <= end) {
+      numbers.push(index);
+      index++;
     }
 
-    describe('Container Inheritance', () => {
-        it.each(
-            createArrayOfNumbers(15)
-        )('should deeply inherit services where depth is %i', (number) => {
-            const NAME = new Token<string>();
-            const name = 'Joanna';
-    
-            Container.setValue(NAME, name);
-            const deepChildContainer = createDeepContainerTree(number);
-    
-            expect(deepChildContainer.has(NAME)).toStrictEqual(true);
-            expect(deepChildContainer.getOrNull(NAME)).toStrictEqual(name);
-        });
+    return numbers;
+  }
+
+  describe('Container Inheritance', () => {
+    it.each(createArrayOfNumbers(15))('should deeply inherit services where depth is %i', number => {
+      const NAME = new Token<string>();
+      const name = 'Joanna';
+
+      Container.setValue(NAME, name);
+      const deepChildContainer = createDeepContainerTree(number);
+
+      expect(deepChildContainer.has(NAME)).toStrictEqual(true);
+      expect(deepChildContainer.getOrNull(NAME)).toStrictEqual(name);
     });
-})
+  });
+});
