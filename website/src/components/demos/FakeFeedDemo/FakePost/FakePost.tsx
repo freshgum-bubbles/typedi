2f9646d311423b09124ee2f61e6081631656890b
freshgum-bubbles <freshgum.bubbles@protonmail.com>
2023-07-19 00:26:55 +0100

    fix(website): make live examples responsive

--- website/src/components/demos/FakeFeedDemo/FakePost/FakePost.tsx
+++ website/src/components/demos/FakeFeedDemo/FakePost/FakePost.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import { Post } from "@site/src/services/fake-feed-demo/posts.token";
+import { Post } from '@site/src/services/fake-feed-demo/posts.token';
 
 import styles from './FakePost.module.css';
 
 export interface Props {
-    post: Post;
-    key?: React.Key;
+  post: Post;
+  key?: React.Key;
+  isListItem?: boolean;
 }
 
-export function FakePost ({ post, key }: Props) {
-    return (
-        <div className={styles.fakePost} key={key}>
-            <p className={styles.messageContent}>
-               <span className={styles.messageAuthor}>{post.username}</span> 
-               <span className={styles.messageContent}>{post.post}</span>
-            </p>
-        </div>
-    )
+export function FakePost({ post, key, isListItem = false }: Props) {
+  return (
+    <div
+      className={styles.fakePost}
+      key={key}
+      role="listitem"
+      aria-label="Example of a post made using the FeedService"
+    >
+      <p className={styles.messageContent}>
+        <span className={styles.messageAuthor}>{post.username}</span>
+        <span className={styles.messageContent}>{post.post}</span>
+      </p>
+    </div>
+  );
 }
