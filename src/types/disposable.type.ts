5ecca72e1146841d99b3c317102279d7aaa9f333
freshgum-bubbles <freshgum.bubbles@protonmail.com>
2023-07-26 00:00:06 +0100

    docs(disposable): add TSDoc documentation

--- src/types/disposable.type.ts
+++ src/types/disposable.type.ts
@@ -1,4 +1,30 @@
+/**
+ * An object containing both a {@link Disposable.disposed | disposed} property,
+ * alongside a {@link Disposable.dispose | dispose()} method, which allows for
+ * disposal of the object.
+ */
 export interface Disposable {
+  /** Whether the object has already been disposed. */
   disposed: boolean;
+
+  /**
+   * Dispose the object.
+   * 
+   * @returns Either void, or a promise which settles upon
+   * completion of the disposal process.
+   * 
+   * @throws Error
+   * This exception may be thrown in the case of an error
+   * encountered during the process of disposing the object.
+   * The type of error thrown is unknown.
+   * 
+   * @privateRemarks
+   * As this is a semi-internal type which we cast objects to,
+   * we have no control over the return type. Thus, the return
+   * type of the dispose call could either be a promise or void.
+   * In the case of a promise, it is generally expected that the
+   * caller would await the process to settle before continuing
+   * other operations or returning control to the host context.
+   */
   dispose(): Promise<void> | void;
 }
