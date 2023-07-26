d46c96554e289ba7e41a901de4efc15b37ca48d6
freshgum-bubbles <freshgum.bubbles@protonmail.com>
2023-07-26 19:13:00 +0100

    docs(types): mark AnyConstructable as public

--- src/types/any-constructable.type.ts
+++ src/types/any-constructable.type.ts
@@ -3,5 +3,6 @@ import { Constructable } from './constructable.type';
 
 /**
  * A type matching any form of constructable.
+ * @public
  */
 export type AnyConstructable<T = unknown> = Constructable<T> | AbstractConstructable<T>;
