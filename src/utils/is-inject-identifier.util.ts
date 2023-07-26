e15bb8b5fa5ee085cd5af7e7cddbe596679385d5
freshgum-bubbles <freshgum.bubbles@protonmail.com>
2023-07-26 19:12:30 +0100

    docs(utils): add isInjectedFactory TSDoc, mark as private
    TODO: The file name is a bit off?

--- src/utils/is-inject-identifier.util.ts
+++ src/utils/is-inject-identifier.util.ts
@@ -1,7 +1,10 @@
 import { INJECTED_FACTORY } from '../constants/type-stamps.const';
 import { InjectedFactory } from '../types/inject-identifier.type';
 
-/** Check if the specified object is an InjectedFactory. */
+/**
+ * Check if the specified object is an {@link InjectedFactory}. 
+ * @private
+ */
 export function isInjectedFactory(x: object): x is InjectedFactory {
   return (x as any)[INJECTED_FACTORY] === true;
 }
