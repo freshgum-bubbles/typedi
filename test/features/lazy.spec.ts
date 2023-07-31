ebb594a20c970aa650adb0fe907001cde38ee3e5
freshgum <freshgum.bubbles@protonmail.com>
2023-08-01 00:54:21 +0100

    test(lazy): update tests and add tests for usage with constraints

--- test/features/lazy.spec.ts
+++ test/features/lazy.spec.ts
@@ -1,7 +1,12 @@
-import { Container, Lazy, Service } from 'internal:typedi';
+import { Container, Lazy, Service, ServiceIdentifier, SkipSelf, Token } from 'internal:typedi';
 
 describe('Lazy()', function () {
-  let emptyFunction = () => {};
+  /**
+   * Create an empty function just to test the interface.
+   * This would obviously never work in usage.
+   */
+  let emptyFunction = (): ServiceIdentifier => undefined as unknown as ServiceIdentifier;
+
   it('should accept a function and return an object', () => {
     expect(() => Lazy(emptyFunction)).not.toThrowError();
     expect(Lazy(emptyFunction)).toBeInstanceOf(Object);
@@ -17,5 +22,30 @@ describe('Lazy()', function () {
     }
 
     expect(Container.get(MyService).receivedValue).toBeInstanceOf(AnotherService);
+  });
+
+  it('should be compatible with resolution constraints', () => {
+    const LOCATION = new Token<Location>();
+    
+    enum Location {
+      Parent,
+      Child
+    }
+
+    @Service([
+      [Lazy(() => LOCATION), SkipSelf()],
+      Lazy(() => LOCATION)
+    ])
+    class MyService {
+      constructor(public skipSelfLocation: Location, public selfLocation: Location) {}
+    }
+
+    Container.setValue(LOCATION, Location.Parent);
+
+    const childContainer = Container.ofChild(Symbol());
+    childContainer.setValue(LOCATION, Location.Child);
+
+    expect(childContainer.get(MyService).skipSelfLocation).toStrictEqual(Location.Parent);
+    expect(childContainer.get(MyService).selfLocation).toStrictEqual(Location.Child);
   });
 });
