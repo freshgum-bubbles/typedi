058ba3f155a7a8902302ce0a245671715e184018
freshgum <freshgum.bubbles@protonmail.com>
2023-08-01 01:20:07 +0100

    test(transientref): add test skeleton

---
+++ test/features/transient-ref.spec.ts
@@ -0,0 +1,19 @@
+describe.skip('Transient References', () => {
+    describe.skip('TransientRef', () => {
+        it.skip('should be a function', () => {
+
+        });
+
+        it.skip('should accept a ServiceIdentifier', () => {
+
+        });
+
+        it.skip('should be usable as a service dependency', () => {
+
+        });
+
+        it.skip('should work correctly with resolution constraints', () => {
+            
+        });
+    });
+});
