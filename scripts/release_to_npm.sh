2fedd9251dfc9780d3ff2ea6c480bf526fafc9bf
freshgum <freshgum.bubbles@protonmail.com>
2023-08-06 23:55:42 +0100

    chore(scripts): stop copying package.json to build/

--- scripts/release_to_npm.sh
+++ scripts/release_to_npm.sh
@@ -1,5 +1,1 @@
-cp LICENSE build/LICENSE
-cp README.md build/README.md   
-
-jq 'del(.devDependencies) | del(.scripts)' package.json > build/package.json
 pnpm publish --access public --no-git-checks --force
