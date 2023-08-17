a1976628877343fd84a9032a2ebde8c5bd68a44b
freshgum <freshgum.bubbles@protonmail.com>
2023-08-18 00:24:59 +0100

    docs(types): move ServiceIdentifierLocation TSDoc into individual enum members

--- src/types/service-identifier-location.type.ts
+++ src/types/service-identifier-location.type.ts
@@ -3,29 +3,6 @@
  * perspective of the container upon which the location retrieval was performed.
  * @internal
  *
- * ## {@link ServiceIdentifierLocation.Local | Local}
- *
- * In the context of a given container, "local" denotes that the specified
- * identifier can be resolved inside said container, without any hierarchical
- * lookups in the container's parent chain.
- *
- * ## {@link ServiceIdentifierLocation.Parent | Parent}
- *
- * This symbol denotes that the container was not able to find a definition
- * for the identifier locally, but its parent was.  In this context, it is
- * important to remember that the use of the word "parent" does not necessarily
- * mean that the container's direct parent has it -- in fact, it may mean...
- *
- *   1. The parent was able to resolve the identifier directly.
- *   2. The parent searched its parent, and continued until the identifier
- *      was found in the hierarchical tree of parents.
- *
- * ## {@link ServiceIdentifierLocation.None | None}
- *
- * This symbol denotes that the specified identifier could not be found in the
- * context of a given container.  If that container has a parent, this means
- * that its parent (and its parent, etc.) was not able to find the identifier.
- *
  * @remarks
  * This is a constant enumerable.
  * As such, any references to it are inlined at compile-time.
@@ -37,7 +14,29 @@
  * ```
  */
 export const enum ServiceIdentifierLocation {
+  /**
+   * In the context of a given container, "local" denotes that the specified
+   * identifier can be resolved inside said container, without any hierarchical
+   * lookups in the container's parent chain.
+   */
   Local = 0,
+
+  /**
+   * This symbol denotes that the container was not able to find a definition
+   * for the identifier locally, but its parent was.  In this context, it is
+   * important to remember that the use of the word "parent" does not necessarily
+   * mean that the container's direct parent has it -- in fact, it may mean...
+   *
+   *   1. The parent was able to resolve the identifier directly.
+   *   2. The parent searched its parent, and continued until the identifier
+   *      was found in the hierarchical tree of parents.
+   */
   Parent,
+
+  /**
+   * This symbol denotes that the specified identifier could not be found in the
+   * context of a given container.  If that container has a parent, this means
+   * that its parent (and its parent, etc.) was not able to find the identifier.
+   */
   None,
 }
