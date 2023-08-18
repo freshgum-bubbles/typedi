445e1fe1e559124e68d7416c07972e6bccb7d8cf
freshgum <freshgum.bubbles@protonmail.com>
2023-08-18 22:16:28 +0100

    feat(ci): mangle certain class members in minified output

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,6 +3,34 @@ import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
 import mergeObjects from 'deepmerge';
 
+/**
+ * A list of properties in the codebase to mangle.
+ * In this case, "mangling" refers to the minification of the names in the final bundle.
+ * 
+ * The addition of members to this array should be done very cautiously.
+ * 
+ * WARNING: DO NOT ADD ANY MEMBERS OF PUBLICLY-EXPORTED CLASSES / OBJECTS HERE!!!
+ * A special exception is made for `ContainerInstance.throwIfDisposed`, which is
+ * marked as private and can easily be replicated.
+ */
+const PROPERTIES_TO_MANGLE = [
+  // ContainerInstance
+  'throwIfDisposed',
+
+  //VisitorCollection
+  'notifyChildContainerVisited',
+  'notifyChildContainerVisited',
+  'notifyOrphanedContainerVisited',
+  'notifyNewServiceVisited',
+  'notifyRetrievalVisited',
+  'notifyVisitors',
+  'forwardOrphanedContainerEvents',
+  'anyVisitorsPresent',
+  'forEachVisitor',
+  'addVisitorToCollection',
+  'removeVisitorFromCollection'
+];
+
 /** @type {import('@rollup/plugin-terser').Options} */
 const terserOptions = {
   compress: {
@@ -15,6 +43,11 @@ const terserOptions = {
     ecma: 6,
     ie8: false,
     comments: false
+  },
+  mangle: {
+    properties: {
+      regex: new RegExp(PROPERTIES_TO_MANGLE.join('|'))
+    }
   }
 };
 
