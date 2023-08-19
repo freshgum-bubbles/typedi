d35f5eec2d4e86030dc5a3bbdb0a2201fca7ade5
freshgum <freshgum.bubbles@protonmail.com>
2023-08-19 08:57:30 +0100

    test(container): test .set without type or dependencies

---
+++ test/features/service-options-without-dependencies.spec.ts
@@ -0,0 +1,14 @@
+import {
+    Container,
+    Token,
+} from '../../src/index';
+
+describe('Container.set without dependencies', () => {
+    it('should provide the appropriate overload and function correctly', () => {
+        const NAME = new Token<string>();
+
+        Container.set({ id: NAME, value: 'Joanna' });
+        expect(Container.has(NAME)).toBe(true);
+        expect(Container.get(NAME)).toStrictEqual('Joanna');
+    });
+});
