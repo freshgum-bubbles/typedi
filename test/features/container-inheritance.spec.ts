cdbc22b28f23d9737593d88b2d9dd78ce49a7418
freshgum <freshgum.bubbles@protonmail.com>
2023-09-05 03:25:02 +0100

    feat(tests): extract createDeepContainerTree to module

--- test/features/container-inheritance.spec.ts
+++ test/features/container-inheritance.spec.ts
@@ -1,19 +1,8 @@
 import { Container, Token } from '../../src/index';
 import { createArrayOfNumbers } from '../utils/create-array-of-numbers.util';
+import { createDeepContainerTree } from '../utils/create-deep-container-tree.util';
 
 describe('Child Containers', () => {
-  function createDeepContainerTree(levels: number, root = Container) {
-    let container = root;
-    let index = 0;
-
-    while (index <= levels) {
-      container = container.ofChild(Symbol());
-      index++;
-    }
-
-    return container;
-  }
-
   describe('Container Inheritance', () => {
     it.each(createArrayOfNumbers(15))('should deeply inherit services where depth is %i', number => {
       const NAME = new Token<string>();
