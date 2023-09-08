4820ca72800f7f871b4e6c4db3b1972601194e38
freshgum <freshgum.bubbles@protonmail.com>
2023-09-09 00:31:15 +0100

    docs(typedservice): fix typo in TSDoc

---
+++ test/features/contrib/typed-service.spec.ts
@@ -0,0 +1,11 @@
+import { TypedService } from '../../../src/contrib/typed-service.decorator';
+
+describe('TypedService', () => {
+    it('is a function', () => {
+        expect(typeof TypedService).toBe('function');
+    });
+
+    it('registers the class in the specified container', () => {
+        
+    });
+});
