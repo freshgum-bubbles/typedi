3b9556e30a62dc149a0f7f151dc285892d713c56
freshgum <freshgum.bubbles@protonmail.com>
2023-09-09 00:31:40 +0100

    test(typedservice): remove duplicated test cases

--- test/features/contrib/typed-service.spec.ts
+++ test/features/contrib/typed-service.spec.ts
@@ -1,11 +1,5 @@
 import { TypedService } from '../../../src/contrib/typed-service.decorator';
 
 describe('TypedService', () => {
-    it('is a function', () => {
-        expect(typeof TypedService).toBe('function');
-    });
-
-    it('registers the class in the specified container', () => {
-        
-    });
+    
 });
