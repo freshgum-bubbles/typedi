0cb9ad09a979c05aecc34f68e938d14df1ceddb0
freshgum <freshgum.bubbles@protonmail.com>
2023-09-17 19:23:37 +0100

    fix(tests): add skeleton ES Decorators suite

--- test/decorators/service-es/Service.spec.ts
+++ test/decorators/service-es/Service.spec.ts
@@ -1,3 +1,7 @@
+import { Service } from 'internal:typedi';
+
 describe('Service Decorator (ES)', () => {
-    
+  it('should be a function', () => {
+    expect(typeof Service).toStrictEqual('function');
+  });
 });
