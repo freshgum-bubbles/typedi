98fe745fe6db1f0dfafb2869369fa0a54bb0f4e7
freshgum <freshgum.bubbles@protonmail.com>
2023-09-18 02:25:22 +0100

    fix(docs/website): set correct URL

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -52,10 +52,10 @@ const config = {
   ],
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://typedi.js.org/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/typedi/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
