6bed06871fa8e460f6245dcdf84dbe65dd96b271
freshgum <freshgum.bubbles@protonmail.com>
2023-10-09 13:27:52 +0100

    docs(website): update TypeDoc configuration
    Update the TypeDoc configuration to make it work
    with the latest version(s) of TypeDI.

--- typedoc.config.js
+++ typedoc.config.js
@@ -2,13 +2,23 @@ const { repository, name } = require('./package.json');
 
 /** @type {import('typedoc').TypeDocOptions} */
 var configuration = {
-  entryPoints: ['./src/index.ts'],
-  gitRemote: repository,
+  /**
+   * The barrel file exports everything consumers should currently know about.
+   * Perhaps in future we may want to also document contrib/ packages.
+   */
+  entryPoints: ['./src/index.mts'],
+  gitRemote: repository.url,
+
+  /** Use the mxssfd theme. */
   plugin: ['@mxssfd/typedoc-theme'],
   theme: 'my-theme',
+
+  name,
+
+  /** The README is included in the index.html page. */
   readme: 'README.md',
   tsconfig: 'tsconfig.typedoc.json',
-  out: './website/static/api-reference/',
+  out: './docs/static/api-reference/',
   pretty: true,
   titleLink: repository.url,
 };
