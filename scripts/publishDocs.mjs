1f684047f68dd23745bf4c3c62e7748ff2e76153
freshgum <freshgum.bubbles@protonmail.com>
2023-10-09 14:31:40 +0100

    feat(scripts): refactor, cleanup & finalize

--- scripts/publishDocs.mjs
+++ scripts/publishDocs.mjs
@@ -1,12 +1,17 @@
+// @ts-check
+
 import { config } from 'dotenv';
-import { resolve as resolvePath } from 'node:path';
+import { resolve as resolvePath, dirname } from 'node:path';
 import assert from 'node:assert';
 import { $ } from 'execa';
+import { fileURLToPath } from 'node:url';
+
+const scriptsDirectory = dirname(fileURLToPath(import.meta.url));
+const dotenvFilePath = resolvePath(scriptsDirectory, '../.env');
+const docsDirectoryPath = resolvePath(scriptsDirectory, '../docs/');
 
 /** Load variables from the .env file into `process.env`. */
-config({
-    path: resolvePath(__dirname, '../.env')
-});
+config({ path: dotenvFilePath });
 
 /**
  * Pipe the stderr and stdout of an execa child process to this process' streams.
@@ -14,15 +19,33 @@ config({
  * @param {import('execa').ExecaChildProcess} childProcess The script to act upon.
  * @param {boolean} interactive Whether stdin is also forwarded.
  */
-function pipeChildProcessIO (childProcess, interactive) {
+function pipeChildProcessIO (childProcess, interactive = false) {
     const { stdout, stderr, stdin } = childProcess;
 
-    stdout.pipe(process.stdout);
-    stderr.pipe(process.stderr);
+    stdout?.pipe(process.stdout);
+    stderr?.pipe(process.stderr);
     
     if (interactive) {
-        stdin.pipe(process.stdin);
+        stdin?.pipe(process.stdin);
     }
+}
+
+/**
+ * Monitor the exit code of a child.
+ * 
+ * @param {import('execa').ExecaChildProcess} childProcess The script to act upon.
+ * @param {boolean} exitOnError If the child exits with an error code, this process is terminated
+ * with the exit code of the child.
+ */
+async function monitorChildProcessExitCode (childProcess, exitOnError = true) {
+    const { exitCode } = await childProcess;
+    const failed = exitCode > 0;
+
+    if (failed && exitOnError) {
+        process.exit(exitCode);
+    }
+
+    return failed;
 }
 
 /**
@@ -46,13 +69,44 @@ const gitPass = getEnvironmentalVariable('DOCUSAURUS_GIT_PASS', true);
 
 // TODO: check for any dirty / staged git files?
 
-const deployScript = $({
-    env: {
-        GIT_USER: gitUser,
-        GIT_PASS: gitPass
-    }
-})`npm run docs:website`;
+await buildAPIReference();
+await buildDocs();
+await deployDocs();
 
-/** Pipe the deploy script's std[in,out,err] to this process'. */
-pipeChildProcessIO(deployScript);
+async function deployDocs() {
+    const deployDocsCommand = $({
+        env: {
+            GIT_USER: gitUser,
+            GIT_PASS: gitPass
+        },
+
+        /** Run in docs/. Alternative to `pushd` which isn't cross-platform. */
+        cwd: docsDirectoryPath
+    })`npm run deploy`;
+
+    /** Docusaurus' deploy script *might* need stdin input; not 100% sure whether this is needed. */
+    pipeChildProcessIO(deployDocsCommand, true);
+
+    await monitorChildProcessExitCode(deployDocsCommand);
+}
+
+async function buildDocs() {
+    const startDocsBuildCommand = $({
+        /** Run in docs/. Alternative to `pushd` which isn't cross-platform. */
+        cwd: docsDirectoryPath
+    })`npm run build`;
+
+    /** Pipe the deploy script's std[in,out,err] to this process'. */
+    pipeChildProcessIO(startDocsBuildCommand);
+
+    await monitorChildProcessExitCode(startDocsBuildCommand);
+}
+
+async function buildAPIReference() {
+    const buildApiReferenceCommand = $`npm run docs:api-reference`;
+    pipeChildProcessIO(buildApiReferenceCommand);
+
+    /** Let TypeDoc move its files into docs/static before running Docusaurus. */
+    await monitorChildProcessExitCode(buildApiReferenceCommand);
+}
 
