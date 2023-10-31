60d9bdbe9078dd227e4d6e1821c7696dcf479baa
freshgum <freshgum.bubbles@protonmail.com>
2023-10-31 22:28:29 +0000

    feat(rollup): add bundle preamble text
    Note that this is not actually used by Rollup yet.

---
+++ scripts/rollup/bundlePreamble.js
@@ -0,0 +1,25 @@
+/**
+ *   _____   ___                __                        __ ______                 ___   ____
+ *  / ___ \ / _/____ ___  ___  / /  ___ _ __ __ __ _    _/_//_  __/__ __ ___  ___  / _ \ /  _/
+ * / / _ `// _// __// -_)(_-< / _ \/ _ `// // //  ' \ _/_/   / /  / // // _ \/ -_)/ // /_/ /
+ * \ \_,_//_/ /_/   \__//___//_//_/\_, / \_,_//_/_/_//_/    /_/   \_, // .__/\__//____//___/
+ *  \___/                         /___/                          /___//_/
+ * <%= pkg.description %>
+ * @version <%= pkg.version %>
+ * @fileoverview
+ *
+ * @module <%= pkg.name %>
+ * @author <%= pkg.author %>
+ *
+ * @license <%= pkg.license %>
+ *
+ * For more information on how to use TypeDI, check out the project's GitHub
+ * repository, along with the dedicated documentation website.
+ * @see <%= pkg.repository.url %>
+ * @see <%= pkg.homepage %>
+ *
+ * ## License
+ * ```
+ * <%= data.licenseText.split('\n').join('\n * ') %>
+ * ```
+ */
