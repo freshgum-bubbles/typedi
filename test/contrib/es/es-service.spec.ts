7da4aa33f232169d50c1fa102b4423b25991dbda
freshgum <freshgum.bubbles@protonmail.com>
2023-11-29 21:11:34 +0000

    style(tests): format with Prettier
    Preemptive Prettier scan to avoid CI.

--- test/contrib/es/es-service.spec.ts
+++ test/contrib/es/es-service.spec.ts
@@ -1,6 +1,6 @@
-import { Container } from "internal:typedi";
-import { createFakeClassDecoratorContext } from "./utils/fake-context.util";
-import { ESService } from "internal:typedi/contrib/es/es-service.decorator.mjs";
+import { Container } from 'internal:typedi';
+import { createFakeClassDecoratorContext } from './utils/fake-context.util';
+import { ESService } from 'internal:typedi/contrib/es/es-service.decorator.mjs';
 
 // Due to the testing structure, we can't actually use ES Decorators
 // here.  Instead, we have to pretend we're using them, by manually
@@ -8,14 +8,14 @@ import { ESService } from "internal:typedi/contrib/es/es-service.decorator.mjs";
 // P.S. The actual conformance testing goes on in <test/decorators/Service.spec.ts>
 // This suite just tests parts of required ESService-specific testing.
 describe('ESService', () => {
-    beforeEach(() => Container.reset({ strategy: 'resetValue' }));
+  beforeEach(() => Container.reset({ strategy: 'resetValue' }));
 
-    // Note: in <test/contrib/es/utils/es-service-decorator-wrapper.util.ts>, we already test
-    // if the implementation accepts valid invocations with valid ClassDecoratorContext objects.
-    it('should fail if not passed a ClassDecoratorContext', () => {
-        class MyService { }
+  // Note: in <test/contrib/es/utils/es-service-decorator-wrapper.util.ts>, we already test
+  // if the implementation accepts valid invocations with valid ClassDecoratorContext objects.
+  it('should fail if not passed a ClassDecoratorContext', () => {
+    class MyService {}
 
-        const context = createFakeClassDecoratorContext(MyService);
-        expect(() => ESService([ ])(class { }, context)).not.toThrow();
-    });
+    const context = createFakeClassDecoratorContext(MyService);
+    expect(() => ESService([])(class {}, context)).not.toThrow();
+  });
 });
