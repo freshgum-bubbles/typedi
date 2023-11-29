13a4188674cba533c68ddcbcfb2c3c3c9ff70a65
freshgum <freshgum.bubbles@protonmail.com>
2023-11-29 21:10:40 +0000

    test(contrib/es): add testing for ESService decorator
    This marks a pretty good milestone for development.
    We've got the Service test suite passing with ESService,
    and we've also ensured a few other things in the
    ESService-specific testing suite.

---
+++ test/contrib/es/test-utils/fake-context.util.ts
@@ -0,0 +1,12 @@
+export function createFakeClassDecoratorContext<TFunction extends Function>(targetConstructor: TFunction): ClassDecoratorContext<abstract new (...args: any) => any> {
+    return {
+        addInitializer(initializer) {
+            // We can't implement this, and it isn't used by ESService.
+            throw new Error('Not implemented.');
+        },
+        kind: 'class',
+        // TODO: Not sure what goes here, and it isn't used by ESService.
+        metadata: {},
+        name: targetConstructor.name
+    };
+}
