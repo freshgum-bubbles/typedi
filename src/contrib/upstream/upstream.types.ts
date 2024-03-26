6e20f21166d0ecd8b5dee8fb7cedbf84ec73cde5
freshgum <freshgum.bubbles@protonmail.com>
2024-03-26 01:44:54 +0000

    feat(contrib/upstream): add barrels

---
+++ src/contrib/upstream/upstream.types.ts
@@ -0,0 +1,17 @@
+export {
+    CannotInjectValueError as UpstreamCannotInjectValueError,
+    CannotInstantiateValueError as UpstreamCannotInstantiateValueError,
+    Constructable as UpstreamConstructable,
+    Container as UpstreamContainer,
+    ContainerInstance as UpstreamContainerInstance,
+    Handler as UpstreamHandler,
+    Inject as UpstreamInject,
+    InjectMany as UpstreamInjectMany,
+    Service as UpstreamService,
+    ServiceIdentifier as UpstreamServiceIdentifier,
+    ServiceMetadata as UpstreamServiceMetadata,
+    ServiceNotFoundError as UpstreamServiceNotFoundError,
+    ServiceOptions as UpstreamServiceOptions,
+    Token as UpstreamToken
+} from './tree/src/index';
+
