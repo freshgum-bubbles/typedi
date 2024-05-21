6883a17a62c7c1c6b9b630b360aec145a4dbd005
freshgum <freshgum.bubbles@protonmail.com>
2024-05-21 11:00:23 +0100

    test(core): move etoken tests to features/

--- test/features/executable-token.spec.ts
+++ test/features/executable-token.spec.ts
@@ -2,7 +2,7 @@ import { inspect } from 'node:util';
 import { ContainerInstance, HostContainer, Token } from 'internal:typedi';
 import { ExecutableToken, isExecutableToken } from 'internal:typedi/executable-token.class.mjs';
 import { EXECUTABLE_TOKEN, ExecutableTokenStamp } from 'internal:typedi/constants/stamps/executable-token.const.mjs';
-import { hasOwnProperty } from './utils/has-own-property.util';
+import { hasOwnProperty } from '../utils/has-own-property.util';
 
 class MyExecutableToken extends ExecutableToken<string> {
   execute(subject: ContainerInstance): string {
