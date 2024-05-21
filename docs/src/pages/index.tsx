720e3d8288e348d4b585d2edac0f308b70ad3147
freshgum <freshgum.bubbles@protonmail.com>
2024-05-21 10:54:46 +0100

    docs(website): move tutorial to /docs/tutorial/

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--primary button--lg"
-            to="/docs/getting-started">
+            to="/docs/tutorial/getting-started">
             Getting Started
           </Link>
           <Link
