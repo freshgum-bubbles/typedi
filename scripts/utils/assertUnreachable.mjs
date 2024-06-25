a0b254b4cc7f85cf08f09a2f4084f6117f9e5260
freshgum <freshgum.bubbles@protonmail.com>
2024-06-25 12:19:29 +0100

    feat(core): add web build entry point generator
    Add a script that generates a barrel entry point for
    the web build.  This allows us to pull-in contrib and
    other modules at build-time, as opposed to shipping
    them in separate modules.
    
    Also included is a new tsconfig that allows for the
    referencing of contributory modules without the use
    of a relative path -- this makes it easier to write
    the build script without breaking stuff.

---
+++ scripts/utils/assertUnreachable.mjs
@@ -0,0 +1,12 @@
+import assert from "assert";
+
+/**
+ * Throw an error.
+ *
+ * @param {string | Error | undefined} The message or Error instance to throw.
+ * @returns {never}
+ */
+export function assertUnreachable (message) {
+    // TODO: This is an awful default.
+    assert(false, message ?? 'An unreachable code-point was reached.');
+}
