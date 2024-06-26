6e4ba0df1abd6a4266c3de8af911732d71c20a3b
freshgum <freshgum.bubbles@protonmail.com>
2024-06-27 00:56:29 +0100

    test(core): add tests for errors

---
+++ test/errors.spec.ts
@@ -0,0 +1,30 @@
+import {
+    CannotInstantiateBuiltInError,
+    CannotInstantiateValueError,
+    ContainerRegistryError,
+    ServiceNotFoundError
+} from 'internal:typedi';
+
+describe.each([
+    CannotInstantiateBuiltInError,
+    CannotInstantiateValueError,
+    ContainerRegistryError,
+    ServiceNotFoundError
+])('$name', (errorCtor) => {
+    it('is a function', () => {
+        expect(typeof errorCtor).toBe('function');
+    });
+
+    it('extends the Error class', () => {
+        expect(Object.create(errorCtor.prototype)).toBeInstanceOf(Error);
+    });
+
+    it('has native Error class static methods', () => {
+        expect(typeof errorCtor.captureStackTrace).toBe('function');
+        expect(typeof errorCtor.prepareStackTrace).toBe('function');
+    });
+});
+
+// TODO: Add tests for identifier normalization here.
+// Surprised the coverage doesn't pick that up, as we never actually construct
+// the classes in this suite.
