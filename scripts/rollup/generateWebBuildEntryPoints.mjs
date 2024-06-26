9798f7fa03361c3fce7c0ba4a75b0ba9ba3fee9b
freshgum <freshgum.bubbles@protonmail.com>
2024-06-26 22:10:04 +0100

    feat(scripts): move Rollup cfg to scripts/, refactor
    The Rollup configuration file has been refactored so it
    is able to create web builds of the Container, which
    include all contributory modules with valid entry-points.
    
    The new configuration file has been refactored to use
    a more FP-style of programming, which has helped to
    reduce the amount of code duplication needed.
    
    The web entry-point generator utility has also been
    refactored to make it easier to use.  Asynchronous
    functions have been adopted in the hopes of allowing
    for a faster build time, though further optimisations
    could probably be made here.
    
    The TypeScript configuration also had to be changed
    to allow for the migration of the rollup.config.mjs
    file to its TypeScript equivalent.  The module
    options had to be changed to Node. This doesn't
    seem to affect anything else.

--- scripts/rollup/generateWebBuildEntryPoints.mjs
+++ scripts/rollup/generateWebBuildEntryPoints.mjs
@@ -1,18 +1,20 @@
 // @ts-check
 
-import { existsSync, readdirSync, statSync, writeFileSync } from 'fs';
-import path, { dirname, resolve } from 'path';
-import rt from 'runtypes';
-import { assertUnreachable } from '../utils/assertUnreachable.mjs';
-import { fileURLToPath } from 'url';
-import { createUuidIterator } from '../utils/uuid.mjs';
+import FS from 'fs';
+import AsyncFS from 'fs/promises';
+import Url from 'url';
+import Path from 'path';
+import Rt from 'runtypes';
 import camelcase from 'camelcase';
 
+import { assertUnreachable } from '../utils/assertUnreachable.mjs';
+import { createUuidIterator } from '../utils/uuid.mjs';
+
 // All hail Node.
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = Path.dirname(Url.fileURLToPath(import.meta.url));
 
 /** The directory in which web build entry points shall be generated. */
-const ENTRY_POINT_DIR = resolve(__dirname, '../../src/entry/web/');
+const ENTRY_POINT_DIR = Path.resolve(__dirname, '../../src/entry/web/');
 
 const ENTRY_POINT_CONTRIB_NAME = 'contrib.full.generated.mts';
 
@@ -23,57 +25,90 @@ const GENERATED_CONTRIB_BARREL_PREAMBLE = `
 const CONTRIB_PACKAGE_ENTRY_POINT_FILE_NAMES = ['index.mts'];
 
 /** The directory in which contributory features are located. */
-const CONTRIB_DIR = resolve(__dirname, '../../src/contrib/');
-const SRC_DIR = resolve(__dirname, '../../src/');
+const CONTRIB_DIR = Path.resolve(__dirname, '../../src/contrib/');
+const SRC_DIR = Path.resolve(__dirname, '../../src/');
 
-/** A map of overrides for certain contributory package names. */
+/**
+ * @type {{ [k: string]: string }}
+ * A map of overrides for certain contributory package names.
+ */
 const PACKAGE_NAME_OVERRIDES = {
   es: 'ES',
 };
 
-const BarrelConfiguration = rt.Record({
-  baseDir: rt.String,
-  exportedPackages: rt.Union(rt.Literal('all'), rt.Array(rt.String)),
-  generatedImports: rt.Record({
-    importPrefix: rt.Union(rt.String, rt.Literal('@typedi:contrib'))
-  })
+export const BarrelConfiguration = Rt.Record({
+  packagesToExport: Rt.Union(
+    // All packages are exported.
+    Rt.Literal('all'),
+
+    // Only the specified packages are exported.
+    Rt.Array(Rt.String)
+  ),
+  generatedImports: Rt.Record({
+    // The import prefix to use when generating imports for contributory packages.
+    importPrefix: Rt.Union(Rt.String),
+  }),
 });
 
 let GENERATED_EXPORT_UUID_ITERATOR = createUuidIterator();
 const GENERATED_EXPORT_PREFIX = 'GeneratedWebEntryPointExport$';
 
-function getAllContribPackages() {
-  return readdirSync(CONTRIB_DIR).filter(entry => {
-    const fullPath = resolve(CONTRIB_DIR, entry);
-    const stat = statSync(fullPath);
-    const isValidEntry = stat.isDirectory();
+/**
+ * Get a list of all contributory packages.
+ * The returned list of packages is not checked for missing entry-points.
+ */
+export async function* getAllDirsInContribDir() {
+  const dir = await AsyncFS.readdir(CONTRIB_DIR);
 
-    // if (!isValidEntry) {
-    //   console.warn([
-    //     `An invalid contributory package was encountered: "${entry}"`,
-    //     `A directory was expected.`
-    //   ].join('\n'));
-    // }
+  for (const entry of dir) {
+    const fullPath = Path.resolve(CONTRIB_DIR, entry);
+    const stat = await AsyncFS.stat(fullPath);
+    const isDirectory = stat.isDirectory();
+    const isValidEntry = isDirectory;
 
-    return isValidEntry;
-  });
+    if (isValidEntry) {
+      yield entry;
+    } else {
+      console.warn(
+        [`An invalid contributory package was encountered: "${entry}".`, `A directory was expected.`].join('\n')
+      );
+    }
+  }
 }
 
-function getAllContribPackagesWithEntryPoints() {
-  const packages = getAllContribPackages();
+/**
+ * Get a list of all contributory packages.
+ *
+ * Like {@link getAllDirsInContribDir}, but the returned packages are checked
+ * for missing entry-points.
+ *
+ * @returns {Promise<{ [k: string]: string }>} A mapping of contributory package names to
+ * their respective entry-points.  The entry-point file names are named as they appear in
+ * the file system.
+ */
+export async function getAllContribPackagesWithEntryPoints() {
+  const packages = getAllDirsInContribDir();
+  let map = Object.create(null);
 
-  return packages.reduce((map, packageName) => {
-    // This function will throw if it can't find an end-point, so this is fine.
+  for await (const packageName of packages) {
     const entryPoint = getEntryPointForContribPackage(packageName);
-    return { ...map, [packageName]: entryPoint };
-  }, Object.create(null));
+    map = { ...map, [packageName]: entryPoint };
+  }
+
+  return map;
 }
 
-function getEntryPointForContribPackage(contribPackageName) {
-  const fullPackagePath = resolve(CONTRIB_DIR, contribPackageName);
+/**
+ * Determine the entry-point of a given contributory package.
+ * The existence of files is checked synchronously.
+ *
+ * @param {string} contribPackageName - The name of the contributory package to check.
+ */
+export function getEntryPointForContribPackage(contribPackageName) {
+  const fullPackagePath = Path.resolve(CONTRIB_DIR, contribPackageName);
   const matches = CONTRIB_PACKAGE_ENTRY_POINT_FILE_NAMES.filter(possibleEntryPointFileName => {
-    const fullEntryPointFileName = resolve(fullPackagePath, possibleEntryPointFileName);
-    return existsSync(fullEntryPointFileName);
+    const fullEntryPointFileName = Path.resolve(fullPackagePath, possibleEntryPointFileName);
+    return FS.existsSync(fullEntryPointFileName);
   });
 
   if (matches.length === 0) {
@@ -90,7 +125,7 @@ function getEntryPointForContribPackage(contribPackageName) {
     assertUnreachable(
       [
         `Multiple entry-points for the "${contribPackageName}" contributory package were encountered:`,
-        matches.join(', '),
+        ...matches.map(x => `  - ${x}`),
       ].join('\n')
     );
   }
@@ -98,6 +133,14 @@ function getEntryPointForContribPackage(contribPackageName) {
   return matches[0];
 }
 
+/**
+ * Resolve a file-name to its equivalent import file-name in TypeScript.
+ * For instance, `.mts` files must be reference as `.mjs` files.
+ *
+ * @param {string} fileName - The file-name to transform.
+ * @param {string} contribPackageName - The name of the contributory package
+ * currently being resolved.
+ */
 function resolveEntryPointNameToImportName(fileName, contribPackageName) {
   switch (fileName) {
     // As per the local TypeScript config, .mts files must be referenced as .mjs.
@@ -112,8 +155,15 @@ function resolveEntryPointNameToImportName(fileName, contribPackageName) {
   );
 }
 
-function getAllContribPackagesWithImportNames() {
-  const packagesWithEntryPoints = getAllContribPackagesWithEntryPoints();
+/**
+ * Like {@link getAllContribPackagesWithEntryPoints}, but with the entry-point names transformed
+ * to import specifiers via {@link resolveEntryPointNameToImportName}.
+ *
+ * @returns {Promise<{ [k: string]: string }>} A mapping of contributory package names to their
+ * respective entry-points as valid import specifiers.
+ */
+export async function getAllContribPackagesWithImportNames() {
+  const packagesWithEntryPoints = await getAllContribPackagesWithEntryPoints();
 
   return Object.entries(packagesWithEntryPoints).reduce((map, [packageName, entryPoint]) => {
     const importName = resolveEntryPointNameToImportName(entryPoint, packageName);
@@ -124,10 +174,10 @@ function getAllContribPackagesWithImportNames() {
 /**
  * Create a barrel file for the provided contributory packages.
  *
- * @param {rt.Static<typeof BarrelConfiguration>} barrelConfig - The barrel configuration to use.
+ * @param {Rt.Static<typeof BarrelConfiguration>} barrelConfig - The barrel configuration to use.
  */
-function createBarrelFileForContribImports(barrelConfig) {
-  const allPackagesWithImports = getAllContribPackagesWithImportNames();
+export async function createBarrelAsString(barrelConfig) {
+  const allPackagesWithImports = await getAllContribPackagesWithImportNames();
 
   // All generated imports must be prefixed with the import prefix specified.
   // We do this because we can substitute this for "@internal:contrib" in the build
@@ -146,10 +196,27 @@ function createBarrelFileForContribImports(barrelConfig) {
     }, GENERATED_CONTRIB_BARREL_PREAMBLE);
 }
 
+/**
+ * Transform the specified package name into one suitable for exporting.
+ * For instance, `transient-ref` would be transformed to `TransientRef`.
+ *
+ * @param {string} packageName - The package name to transform.
+ * @returns { string } The transformed package name.
+ */
 function getExportNameForPackage(packageName) {
   return PACKAGE_NAME_OVERRIDES[packageName] ?? camelcase(packageName, { pascalCase: true });
 }
 
+/**
+ * Generate a JavaScript snippet containing a star import from the given import specifier,
+ * which is then immediately re-exported as the provided export name.
+ *
+ * @remarks
+ * The syntax of the emitted JavaScript is not checked.
+ *
+ * @param {string} path - The import specifier to re-export.
+ * @param {string} exportName - The name to re-export all exports as.
+ */
 function generateBarrelReexportForPath(path, exportName) {
   const baseId = generateUuidForExport();
   const fullId = `_${exportName}${baseId}`;
@@ -159,40 +226,38 @@ import * as ${fullId} from "${path}";
 export { ${fullId} as ${exportName} };`;
 }
 
+/** Generate a UUID for a re-export. */
 function generateUuidForExport() {
   // e.g. GeneratedWebEntryPointExport$1
   return `${GENERATED_EXPORT_PREFIX}${GENERATED_EXPORT_UUID_ITERATOR.next().value}`;
 }
 
-/** @param {rt.Static<typeof BarrelConfiguration>} barrelConfig */
-function writeBarrelFileForConfig(barrelConfig, baseOutputPath = null) {
-  const outputPath = baseOutputPath ?? path.resolve(ENTRY_POINT_DIR, ENTRY_POINT_CONTRIB_NAME);
-  console.log({ outputPath });
-  const barrelFile = createBarrelFileForContribImports(barrelConfig);
-  writeFileSync(outputPath, barrelFile);
+/**
+ * Write a barrel file following the provided configuration to the specified output path.
+ *
+ * @param {Rt.Static<typeof BarrelConfiguration>} barrelConfig - The configuration to use to generate the barrel.
+ * @param {string | null} baseOutputPath - The path to output the barrel file to.
+ */
+export async function writeBarrelFileForConfig(barrelConfig, baseOutputPath = null) {
+  const outputPath = baseOutputPath ?? Path.resolve(ENTRY_POINT_DIR, ENTRY_POINT_CONTRIB_NAME);
+  const barrelFile = await createBarrelAsString(barrelConfig);
+  return AsyncFS.writeFile(outputPath, barrelFile);
 }
 
 /**
  * Determine whether a given barrel configuration excludes a certain package from being exported.
  *
- * @param {rt.Static<typeof BarrelConfiguration>} barrelConfig
- * @param {string} packageName
+ * @param {Rt.Static<typeof BarrelConfiguration>} barrelConfig
+ * @param {string} packageName - The name of the package to test.
+ *
  * @returns {boolean} Whether the package is excluded.
  */
 function doesBarrelConfigExcludePackageName(barrelConfig, packageName) {
-  const { exportedPackages } = barrelConfig;
+  const { packagesToExport } = barrelConfig;
 
-  if (exportedPackages === 'all') {
+  if (packagesToExport === 'all') {
     return false;
   }
 
-  return exportedPackages.some(exportedPackage => exportedPackage === packageName);
+  return packagesToExport.some(exportedPackage => exportedPackage === packageName);
 }
-
-writeBarrelFileForConfig({
-  baseDir: SRC_DIR,
-  exportedPackages: 'all',
-  generatedImports: {
-    importPrefix: 'internal:contrib'
-  }
-});
