4c76133d3a94e119d5b4d44846213df42d3010a5
freshgum <freshgum.bubbles@protonmail.com>
2024-08-04 01:40:29 +0100

    style(test): prettier Jest config

--- jest.config.js
+++ jest.config.js
@@ -11,9 +11,7 @@ const configuration = {
     '^.+\\.(m?[tj]sx?)$': ['ts-jest', { tsconfig: TYPESCRIPT_CONFIGURATIONS.Spec }],
   },
   resolver: '<rootDir>/scripts/testing/jest/moduleResolver.cjs',
-  testPathIgnorePatterns: [
-    'src/contrib/upstream'
-  ]
+  testPathIgnorePatterns: ['src/contrib/upstream'],
 };
 
 module.exports = configuration;
