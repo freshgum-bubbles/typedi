819e4396178a1df3898290d0a28c51abca4340e8
freshgum <freshgum.bubbles@protonmail.com>
2025-05-19 16:31:15 +0100

    fix(test): update wrapped ESService util types

--- test/contrib/es/test-utils/es-service-decorator-wrapper.util.ts
+++ test/contrib/es/test-utils/es-service-decorator-wrapper.util.ts
@@ -5,18 +5,18 @@ import { createFakeClassDecoratorContext } from './fake-context.util';
 // To simplify the implementation, we don't define *each* applicable overload
 // here.  Instead, we define just the implementation and cast the wrapper to
 // <typeof Service>.
-function WrappedESServiceDecoratorImpl<T = unknown>(
+function WrappedESServiceDecoratorImpl<T extends { new (): TReturn }, TReturn>(
   optionsOrDependencies: Omit<ServiceOptions<T>, 'dependencies'> | ServiceOptions<T> | AnyServiceDependency[],
   maybeDependencies?: AnyServiceDependency[]
-): ClassDecorator {
-  return targetConstructor => {
+): { (t: T): void } {
+  return <TTarget extends T>(targetConstructor: TTarget) => {
     const fakeContext: ClassDecoratorContext = createFakeClassDecoratorContext(targetConstructor);
 
     // Note: These aren't guaranteed, just used to trick TypeScript into being quiet.
     ESService<T>(
       optionsOrDependencies as Omit<ServiceOptions<T>, 'dependencies'>,
       maybeDependencies as AnyServiceDependency[]
-    )(targetConstructor as unknown as Constructable<T>, fakeContext);
+    )(targetConstructor, fakeContext);
   };
 }
 
